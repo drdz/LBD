41c5cee2d7559dd342e2183600ac033c57ffcb58
drdz <30946987+drdz@users.noreply.github.com>
2018-05-02 00:49:44 -0500

    Update Practica2.sql

--- Practica 2/Practica2.sql
+++ Practica 2/Practica2.sql
@@ -1,81 +1,81 @@
-﻿USE [master]
+USE [master]
 GO
-/****** Object:  Database [LAB]    Script Date: 04/03/2018 09:11:24 p. m. ******/
-CREATE DATABASE [LAB]
+/****** Object:  Database [Lab]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+CREATE DATABASE [Lab]
  CONTAINMENT = NONE
  ON  PRIMARY 
-( NAME = N'LAB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\LAB.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+( NAME = N'Lab', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\Lab.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
  LOG ON 
-( NAME = N'LAB_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\LAB_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+( NAME = N'Lab_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\Lab_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
 GO
-ALTER DATABASE [LAB] SET COMPATIBILITY_LEVEL = 140
+ALTER DATABASE [Lab] SET COMPATIBILITY_LEVEL = 140
 GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
-EXEC [LAB].[dbo].[sp_fulltext_database] @action = 'enable'
+EXEC [Lab].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
-ALTER DATABASE [LAB] SET ANSI_NULL_DEFAULT OFF 
+ALTER DATABASE [Lab] SET ANSI_NULL_DEFAULT OFF 
 GO
-ALTER DATABASE [LAB] SET ANSI_NULLS OFF 
+ALTER DATABASE [Lab] SET ANSI_NULLS OFF 
 GO
-ALTER DATABASE [LAB] SET ANSI_PADDING OFF 
+ALTER DATABASE [Lab] SET ANSI_PADDING OFF 
 GO
-ALTER DATABASE [LAB] SET ANSI_WARNINGS OFF 
+ALTER DATABASE [Lab] SET ANSI_WARNINGS OFF 
 GO
-ALTER DATABASE [LAB] SET ARITHABORT OFF 
+ALTER DATABASE [Lab] SET ARITHABORT OFF 
 GO
-ALTER DATABASE [LAB] SET AUTO_CLOSE OFF 
+ALTER DATABASE [Lab] SET AUTO_CLOSE OFF 
 GO
-ALTER DATABASE [LAB] SET AUTO_SHRINK OFF 
+ALTER DATABASE [Lab] SET AUTO_SHRINK OFF 
 GO
-ALTER DATABASE [LAB] SET AUTO_UPDATE_STATISTICS ON 
+ALTER DATABASE [Lab] SET AUTO_UPDATE_STATISTICS ON 
 GO
-ALTER DATABASE [LAB] SET CURSOR_CLOSE_ON_COMMIT OFF 
+ALTER DATABASE [Lab] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-ALTER DATABASE [LAB] SET CURSOR_DEFAULT  GLOBAL 
+ALTER DATABASE [Lab] SET CURSOR_DEFAULT  GLOBAL 
 GO
-ALTER DATABASE [LAB] SET CONCAT_NULL_YIELDS_NULL OFF 
+ALTER DATABASE [Lab] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-ALTER DATABASE [LAB] SET NUMERIC_ROUNDABORT OFF 
+ALTER DATABASE [Lab] SET NUMERIC_ROUNDABORT OFF 
 GO
-ALTER DATABASE [LAB] SET QUOTED_IDENTIFIER OFF 
+ALTER DATABASE [Lab] SET QUOTED_IDENTIFIER OFF 
 GO
-ALTER DATABASE [LAB] SET RECURSIVE_TRIGGERS OFF 
+ALTER DATABASE [Lab] SET RECURSIVE_TRIGGERS OFF 
 GO
-ALTER DATABASE [LAB] SET  DISABLE_BROKER 
+ALTER DATABASE [Lab] SET  DISABLE_BROKER 
 GO
-ALTER DATABASE [LAB] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+ALTER DATABASE [Lab] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-ALTER DATABASE [LAB] SET DATE_CORRELATION_OPTIMIZATION OFF 
+ALTER DATABASE [Lab] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-ALTER DATABASE [LAB] SET TRUSTWORTHY OFF 
+ALTER DATABASE [Lab] SET TRUSTWORTHY OFF 
 GO
-ALTER DATABASE [LAB] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+ALTER DATABASE [Lab] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-ALTER DATABASE [LAB] SET PARAMETERIZATION SIMPLE 
+ALTER DATABASE [Lab] SET PARAMETERIZATION SIMPLE 
 GO
-ALTER DATABASE [LAB] SET READ_COMMITTED_SNAPSHOT OFF 
+ALTER DATABASE [Lab] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-ALTER DATABASE [LAB] SET HONOR_BROKER_PRIORITY OFF 
+ALTER DATABASE [Lab] SET HONOR_BROKER_PRIORITY OFF 
 GO
-ALTER DATABASE [LAB] SET RECOVERY SIMPLE 
+ALTER DATABASE [Lab] SET RECOVERY SIMPLE 
 GO
-ALTER DATABASE [LAB] SET  MULTI_USER 
+ALTER DATABASE [Lab] SET  MULTI_USER 
 GO
-ALTER DATABASE [LAB] SET PAGE_VERIFY CHECKSUM  
+ALTER DATABASE [Lab] SET PAGE_VERIFY CHECKSUM  
 GO
-ALTER DATABASE [LAB] SET DB_CHAINING OFF 
+ALTER DATABASE [Lab] SET DB_CHAINING OFF 
 GO
-ALTER DATABASE [LAB] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+ALTER DATABASE [Lab] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-ALTER DATABASE [LAB] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+ALTER DATABASE [Lab] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-ALTER DATABASE [LAB] SET DELAYED_DURABILITY = DISABLED 
+ALTER DATABASE [Lab] SET DELAYED_DURABILITY = DISABLED 
 GO
-ALTER DATABASE [LAB] SET QUERY_STORE = OFF
+ALTER DATABASE [Lab] SET QUERY_STORE = OFF
 GO
-USE [LAB]
+USE [Lab]
 GO
 ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = ON;
 GO
@@ -95,71 +95,167 @@ ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
 GO
 ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
 GO
-USE [LAB]
+USE [Lab]
 GO
-/****** Object:  Table [dbo].[Alumnos]    Script Date: 04/03/2018 09:11:24 p. m. ******/
+/****** Object:  Table [dbo].[Alumno]    Script Date: 02/05/2018 12:44:50 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Alumnos](
+CREATE TABLE [dbo].[Alumno](
 	[Matricula] [uniqueidentifier] NOT NULL,
 	[Nombre] [nvarchar](50) NOT NULL,
-	[Apellidos] [nvarchar](50) NOT NULL,
+	[Apellido paterno] [nvarchar](50) NOT NULL,
+	[Apellido materno] [nvarchar](50) NOT NULL,
+	[Edad] [int] NOT NULL,
 	[Facultad] [nvarchar](50) NOT NULL,
 	[Carrera] [nvarchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Carrera]    Script Date: 04/03/2018 09:11:24 p. m. ******/
+/****** Object:  View [dbo].[Alumno_V]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[Alumno_V] as select Nombre from Alumno
+GO
+/****** Object:  Table [dbo].[a_Area]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[a_Area](
+	[Area] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Area]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Area](
+	[ID empleado] [uniqueidentifier] NOT NULL,
+	[Nombre] [nvarchar](50) NOT NULL,
+	[Apellido paterno] [nvarchar](50) NOT NULL,
+	[Apellido materno] [nvarchar](50) NOT NULL,
+	[Area] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Carrera]    Script Date: 02/05/2018 12:44:50 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Carrera](
 	[ID carrera] [uniqueidentifier] NOT NULL,
-	[Nombre] [nvarchar](50) NOT NULL,
-	[Alumnos] [nvarchar](50) NOT NULL,
-	[Materias] [nvarchar](50) NOT NULL,
-	[Direccion] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
+	[Nombre] [nvarchar](max) NOT NULL,
+	[Acronimo] [nvarchar](50) NOT NULL,
+	[Facultad] [nvarchar](50) NOT NULL,
+	[Campus] [nvarchar](50) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Empleados]    Script Date: 04/03/2018 09:11:24 p. m. ******/
+/****** Object:  Table [dbo].[Empleado]    Script Date: 02/05/2018 12:44:50 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Empleados](
+CREATE TABLE [dbo].[Empleado](
 	[ID empleado] [uniqueidentifier] NOT NULL,
 	[Nombre] [nvarchar](50) NOT NULL,
-	[Apellido] [nvarchar](50) NOT NULL,
+	[Apellido paterno] [nvarchar](50) NOT NULL,
+	[Apellido materno] [nvarchar](50) NOT NULL,
+	[Edad] [int] NOT NULL,
 	[Area] [nvarchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Facultad]    Script Date: 04/03/2018 09:11:24 p. m. ******/
+/****** Object:  Table [dbo].[Facultad]    Script Date: 02/05/2018 12:44:50 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Facultad](
 	[ID facultad] [uniqueidentifier] NOT NULL,
-	[Nombre] [nvarchar](50) NOT NULL,
-	[Carreras] [nvarchar](50) NOT NULL,
-	[Direccion] [nvarchar](50) NOT NULL
-) ON [PRIMARY]
+	[Nombre] [nvarchar](max) NOT NULL,
+	[Acronimo] [nvarchar](50) NOT NULL,
+	[Campus] [nvarchar](50) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Maestros]    Script Date: 04/03/2018 09:11:24 p. m. ******/
+/****** Object:  Table [dbo].[Maestro]    Script Date: 02/05/2018 12:44:50 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Maestros](
+CREATE TABLE [dbo].[Maestro](
 	[ID maestro] [uniqueidentifier] NOT NULL,
 	[Nombre] [nvarchar](50) NOT NULL,
-	[Apellidos] [nvarchar](50) NOT NULL,
-	[Materia] [nvarchar](50) NOT NULL
+	[Apellido paterno] [nvarchar](50) NOT NULL,
+	[Apellido materno] [nvarchar](50) NOT NULL,
+	[Edad] [int] NOT NULL,
+	[Materia] [nvarchar](50) NOT NULL,
+	[Facultad] [nvarchar](50) NOT NULL
 ) ON [PRIMARY]
+GO
+/****** Object:  StoredProcedure [dbo].[consulta_1]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[consulta_1] @nombre nvarchar(50), @facultad varchar(50)
+as
+begin
+	SELECT * FROM Alumno WHERE Nombre= @nombre and Facultad = @facultad;
+	print 'Hecho';
+end
+GO
+/****** Object:  StoredProcedure [dbo].[consulta_2]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[consulta_2] @nombre nvarchar(50), @area nvarchar(50)
+as
+begin
+	SELECT * FROM Empleado WHERE Nombre= @nombre and Area = @area;
+	print 'Hecho';
+end
+GO
+/****** Object:  StoredProcedure [dbo].[consulta_3]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[consulta_3] @nombre nvarchar(50), @materia nvarchar(50)
+as
+begin
+	SELECT * FROM Maestro WHERE Nombre= @nombre and Materia = @materia;
+	print 'Hecho';
+end
+GO
+/****** Object:  StoredProcedure [dbo].[consulta_4]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[consulta_4] @nombre nvarchar(50), @acronimo nvarchar(50)
+as
+begin
+	SELECT * FROM Facultad WHERE Nombre= @nombre and Acronimo = @acronimo;
+	print 'Hecho';
+end
+GO
+/****** Object:  StoredProcedure [dbo].[consulta_5]    Script Date: 02/05/2018 12:44:50 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[consulta_5] @acronimo nvarchar(50), @campus nvarchar(50)
+as
+begin
+	SELECT * FROM Carrera WHERE Acronimo= @acronimo and Campus= @campus
+	print 'Hecho';
+end
 GO
 USE [master]
 GO
-ALTER DATABASE [LAB] SET  READ_WRITE 
+ALTER DATABASE [Lab] SET  READ_WRITE 
 GO
